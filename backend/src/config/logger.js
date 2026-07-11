0bec60bf6d4957ca795e2f0119841ce4beab1b68
Felipe <felipegonc2020@gmail.com>
2026-07-11 12:18:55 -0300

    update logger.js

--- backend/src/config/logger.js
+++ backend/src/config/logger.js
@@ -1,12 +1,16 @@
-import pino from "pino"
+import pino from "pino";
 
-export const logger = pino({
-    transport:{
-        target: "pino-pretty",
-        options:{
+export const logger = pino(
+  process.env.NODE_ENV === "production"
+    ? {}
+    : {
+        transport: {
+          target: "pino-pretty",
+          options: {
             colorize: true,
             translateTime: "HH:MM:ss",
             ignore: "pid"
-        }
-    }
-})
+          },
+        },
+      },
+);
